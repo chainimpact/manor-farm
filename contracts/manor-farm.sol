6ca0e63adcad65adae33aa67e08e2bcdcf48f29b
felipe <felipefaraggi@gmail.com>
2018-08-11 11:21:38 +0200

    reordering to follow standards

--- contracts/manor-farm.sol
+++ contracts/manor-farm.sol
@@ -1,5 +1,4 @@
 pragma solidity ^0.4.24;
-
 /*
 Tracking history of an animals life and general data.
 This can be used for domestic uses such as dog or cats and their vaccines and generic documents as
@@ -8,6 +7,21 @@ well as for more specialized animals such as race horses, livestock and/or exoti
 As a first version, we will focus on domestic animals and add more functionality for livestock
 at a later date.
 */
+
+/*
+TODO:
+add all feeds and pharmaceuticals used in raising the animal
+add location of moements between the animals's origin and place of slaughter (if applicable)
+add movement of specific animal products from the processing plant to the retail consumer (if applicable)
+
+TODO: modify birthdate and deathdate from strings to better data types.
+function set(uint256 _birthdate) {
+    birthdate = _birthdate;
+}
+function set(uint256 _deathdate) {
+    deathdate = _deathdate;
+} */
+
 contract Animals {
 
     string public name;
@@ -24,6 +38,81 @@ contract Animals {
     string public chipId;
     string public tattooId;
 
+    // all the animal's previous owners as well as the
+    // current owner on the last index.
+    address[] public mastersList;
+    // custodians are pre-approved veterinarians, medics, government
+    // appointees and other entities.
+    address[] public custodiansList;
+    // certified custodians at national or international level
+    // TODO: research this subject
+    // TODO: fix `call to Animals.certifiedCustodiansList errored: VM error: invalid opcode`
+    address[] public certifiedCustodiansList;
+
+    // array with list of all medical events done on this animal
+    // ie. a dog has a rabies vaccine injected and this is recorded here
+    MedicalEvent[] public medicalHistory;
+
+    // exotic animals might have fringe cases, so other and none were added
+    enum Sex {male, female, other, none}
+    Sex public sex;
+
+    // all prizes including medals, diplomas, prize money, tropheys, purses,
+    Prize[] public prizesHistory;
+
+    PrizeType private prizeType;
+    /* TODO: research all types of prize types.
+    categories might be fuzzy in which case a variable string
+    might be better suited.*/
+    enum PrizeType {
+        goldMedal,
+        silverMedal,
+        bronzeMedal,
+        otherMedal,
+        diploma,
+        prizeMoney,
+        trophey,
+        purse,
+        award,
+        badge,
+        other
+    }
+
+    struct Prize {
+        /* IPFS url */
+        string eventName;
+        string prizeName;
+        // TODO: add prizeType enum and check if it works
+        // PrizeType prizeType;
+        string prizeType;
+        string date;
+        address masterAtDate;
+        bool certified;
+    }
+
+    /* TODO: research all types of medical events.
+    categories might be fuzzy in which case a variable string
+    might be better suited.*/
+    enum MedicalEventType {
+        vaccine,
+        surgery,
+        pharmacology,
+        sickness,
+        checkup,
+        hospitalization,
+        other
+    }
+    MedicalEventType private medicalEventType;
+
+    struct MedicalEvent {
+        string name;
+        string date;
+        bool ended;
+        address custodian;
+        MedicalEventType medicalEventType;
+        /* string eventType; */
+    }
+    // 0x1115b7d915458ef540ade6068dfe2f44e8fa7111
     // helper string for constructor
     // "0xca35b7d915458ef540ade6068dfe2f44e8fa733c", "spotty", "201010011200", "dog", "terrier", "black", "0xca35b7d915458ef540ade6068dfe2f44e8fa733c", "0xca35b7d915458ef540ade6068dfe2f44e8fa733c", "ab123", "ab123"
     // "spotty","0xca35b7d915458ef540ade6068dfe2f44e8fa733c","0x323031303130303131323030","0","dog","terrier","black","0xca35b7d915458ef540ade6068dfe2f44e8fa733c","0xca35b7d915458ef540ade6068dfe2f44e8fa733c","ab123","ab123"
@@ -55,64 +144,6 @@ contract Animals {
         mastersList.push(_currentMaster);
     }
 
-    /*
-    TODO:
-    add all feeds and pharmaceuticals used in raising the animal
-    add location of moements between the animals's origin and place of slaughter (if applicable)
-    add movement of specific animal products from the processing plant to the retail consumer (if applicable)
-    */
-
-    /*
-    TODO: modify birthdate and deathdate from strings to better data types.
-    function set(uint256 _birthdate) {
-        birthdate = _birthdate;
-    }
-    function set(uint256 _deathdate) {
-        deathdate = _deathdate;
-    } */
-
-    // exotic animals might have fringe cases, so other and none were added
-    enum Sex {male, female, other, none}
-    Sex public sex;
-
-    // all the animal's previous owners as well as the
-    // current owner on the last index.
-    address[] public mastersList;
-    // custodians are pre-approved veterinarians, medics, government
-    // appointees and other entities.
-    address[] public custodiansList;
-    // certified custodians at national or international level
-    // TODO: research this subject
-    // TODO: fix `call to Animals.certifiedCustodiansList errored: VM error: invalid opcode`
-    address[] public certifiedCustodiansList;
-
-    // array with list of all medical events done on this animal
-    // ie. a dog has a rabies vaccine injected and this is recorded here
-    MedicalEvent[] public medicalHistory;
-
-    /* TODO: research all types of medical events.
-    categories might be fuzzy in which case a variable string
-    might be better suited.*/
-    enum MedicalEventType {
-        vaccine,
-        surgery,
-        pharmacology,
-        sickness,
-        checkup,
-        hospitalization,
-        other
-    }
-    MedicalEventType public medicalEventType;
-
-    struct MedicalEvent {
-        string name;
-        string date;
-        bool ended;
-        address custodian;
-        MedicalEventType medicalEventType;
-        /* string eventType; */
-    }
-
     function addMedicalAct(
         string _name,
         string _date,
@@ -121,45 +152,12 @@ contract Animals {
         MedicalEventType _medicalEventType
     ) hasRestrictedAccess() public {
             medicalHistory.push(MedicalEvent({
-                name : _name,
-                date : _date,
-                ended : _ended,
-                custodian : _custodian,
-                medicalEventType : _medicalEventType
+                name: _name,
+                date: _date,
+                ended: _ended,
+                custodian: _custodian,
+                medicalEventType: _medicalEventType
             }));
-    }
-
-    // all prizes including medals, diplomas, prize money, tropheys, purses,
-    Prize[] public prizesHistory;
-
-    /* TODO: research all types of prize types.
-    categories might be fuzzy in which case a variable string
-    might be better suited.*/
-    enum PrizeType {
-        goldMedal,
-        silverMedal,
-        bronzeMedal,
-        otherMedal,
-        diploma,
-        prizeMoney,
-        trophey,
-        purse,
-        award,
-        badge,
-        other
-    }
-    PrizeType public prizeType;
-
-    struct Prize {
-        /* IPFS url */
-        string eventName;
-        string prizeName;
-        // TODO: add prizeType enum and check if it works
-        // PrizeType prizeType;
-        string prizeType;
-        string date;
-        address masterAtDate;
-        bool certified;
     }
 
     /*
@@ -193,27 +191,17 @@ contract Animals {
     }
 
     /*
-    Only custodians have access to some functions/attributes.
-    for example only veterinarians would be able to add medical
-    interventions to an animal's medicalHistory list.
-    Another example would be prize validation. In which only competition
-    organizers can have access.
+    TODO: get cerrtifiedcustodians from external regitry list or TCR
     */
-    function isCertifiedCustodian(address _addr) public view returns (bool) {
-        for (uint i=0; i < certifiedCustodiansList.length; i++) {
-            if (_addr == certifiedCustodiansList[i]) {
-                return true;
-            }
-            return false;
-        }
-    }
+    // function isCertifiedCustodian(address _addr) public view returns (bool) {
+    // }
 
     // either master or custodian have access
     modifier hasRestrictedAccess(){
         require (
             isMaster(msg.sender) ||
-            isCustodian(msg.sender) ||
-            isCertifiedCustodian(msg.sender),
+            isCustodian(msg.sender), // ||
+            // isCertifiedCustodian(msg.sender),
             "This action has restricted access. Must be either current master, custodian or certifiedcustodian."
         );
         _;
@@ -225,7 +213,7 @@ contract Animals {
         mastersList.push(newMaster);
     }
 
-    function addcustodian(address addr) public hasRestrictedAccess() {
+    function addCustodian(address addr) public hasRestrictedAccess() {
         custodiansList.push(addr);
     }
 
@@ -237,6 +225,24 @@ contract Animals {
     // dam is the female parent
     function addDam(address _dam) public hasRestrictedAccess {
         dam = _dam;
+    }
+
+    function addPrize (
+        string _name,
+        string _prizeName,
+        string _prizeType,
+        string _date,
+        address masterAtDate,
+        bool certified
+    ) public {
+        prizesHistory.push(Prize({
+            name: _name,
+            prizeName: _prizeName,
+            prizeType: _prizeType,
+            date: _date,
+            masterAtDate: _masterAtDate,
+            certified: _certified
+        }));
     }
 
     /*
